ef3ea27a1613cc5a0dc9fd97ec743d322fc942af
mauricio <mauricioandresvergarafonseca@gmail.com>
2025-08-31 18:59:31 -0500

    se agregó la dependencia @capacitor/browser y la funcionalidad para abrir artículos de noticias en el navegador

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../shared/services/news.service';
+import { NewsApiResponse, News } from '../../interfaces/news.interface';
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from '../../shared/components/modal/modal.component';
+import { UserService } from '../../shared/services/user.service';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  newsResponse!: NewsApiResponse;
+  principalNews!: News;
+  otherNews!: News[];
+  user!: User;
+  isLoading = true;
 
-  ngOnInit() {
+  constructor(
+    private newsService: NewsService,
+    private modalController: ModalController,
+    private userService: UserService
+  ) { }
+
+  async ngOnInit() {
+    this.user != await this.userService.getCurrentUser();
+    this.loadNews();
   }
 
+  async loadNews(event?) {
+    try {
+      this.isLoading = true;
+      this.newsResponse != await this.newsService.getTopHeadlines().toPromise();
+      
+      if (this.newsResponse && this.newsResponse.articles.length > 0) {
+        this.principalNews = this.newsResponse.articles[0];
+        this.otherNews = this.newsResponse.articles.slice(1);
+      }
+    } catch (error) {
+      console.error('Error al cargar noticias:', error);
+    } finally {
+      this.isLoading = false;
+      if (event) {
+        event.target.complete();
+      }
+    }
+  }
+
+  async openNewsModal(news: News) {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      componentProps: {
+        news: news
+      }
+    });
+    return await modal.present();
+  }
+
+  async doRefresh(event) {
+    await this.loadNews(event);
+  }
 }
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { News } from '../../../interfaces/news.interface';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss'],
+  styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent  implements OnInit {
+export class ModalComponent {
 
-  constructor() { }
+  @Input() news!: News;
 
-  ngOnInit() {}
+  constructor(private modalController: ModalController) { }
 
+  dismiss() {
+    this.modalController.dismiss();
+  }
+
+  async openInBrowser() {
+    await Browser.open({ url: this.news.url });
+  }
 }
